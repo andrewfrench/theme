2ab8837d6fdf43682abbdda9670f140b43ac407b
Andrew French <andrew@afren.ch>
2016-03-22 00:06:45 -0600

    Update mobile view.

--- assets/js/sizer.js
+++ assets/js/sizer.js
@@ -7,36 +7,50 @@
 })();
 
 function sizeBoxes(next) {
-  var realWidth = document.body.clientWidth - 8;
-  var boxes = document.getElementsByClassName('box');
+  if(document.body.clientWidth > 550) {
+    var realWidth = document.body.clientWidth - 8;
+    var boxes = document.getElementsByClassName('box');
 
-  if(boxes) {
-    // Set container width, max 1200px
-    if(realWidth < 1200) {
-      document.getElementById('inner-container').style.width = realWidth + "px";
-    } else {
-      realWidth = 1200;
-      document.getElementById('inner-container').style.width = realWidth + "px";
-    }
+    if(boxes) {
+      // Set container width, max 1200px
+      if(realWidth < 1200) {
+        document.getElementById('inner-container').style.width = realWidth + "px";
+      } else {
+        realWidth = 1200;
+        document.getElementById('inner-container').style.width = realWidth + "px";
+      }
 
-    for(var i = 0; i < boxes.length; i++) {
+      for(var i = 0; i < boxes.length; i++) {
 
-      var classes = boxes[i].className.split(' ');
-      var longDimension = (realWidth / 2) - 8;
-      var shortDimension = (realWidth / 4) - 8;
+        var classes = boxes[i].className.split(' ');
+        var longDimension = (realWidth / 2) - 8;
+        var shortDimension = (realWidth / 4) - 8;
 
-      if(classes.indexOf('big') != -1) {
-        boxes[i].style.width = longDimension + "px";
-        boxes[i].style.height = longDimension + "px";
-      } else if(classes.indexOf('medium') != -1) {
-        boxes[i].style.width = longDimension + "px";
-        boxes[i].style.height = shortDimension + "px";
-      } else if(classes.indexOf('little') != -1) {
-        boxes[i].style.width = shortDimension + "px";
-        boxes[i].style.height = shortDimension + "px";
+        if(classes.indexOf('big') != -1) {
+          boxes[i].style.width = longDimension + "px";
+          boxes[i].style.height = longDimension + "px";
+        } else if(classes.indexOf('medium') != -1) {
+          boxes[i].style.width = longDimension + "px";
+          boxes[i].style.height = shortDimension + "px";
+        } else if(classes.indexOf('little') != -1) {
+          boxes[i].style.width = shortDimension + "px";
+          boxes[i].style.height = shortDimension + "px";
+        }
       }
     }
-  }
+  } else {
+    var realWidth = document.body.clientWidth - 8;
+    var boxes = document.getElementsByClassName('box');
+
+    document.getElementById('inner-container').style.width = realWidth + "px";
+
+    if(boxes) {
+      for(var i = 0; i < boxes.length; i++) {
+        boxes[i].style.width = realWidth + "px";
+        boxes[i].style.height =  "200px";
+        }
+      }
+    }
 
   next();
 }
