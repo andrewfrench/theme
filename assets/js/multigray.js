f6235cfdae7170ec668316bcb0a5125f4c76df63
Andrew French <andrew@afren.ch>
2016-06-04 18:27:55 -0600

    Change the way listed articles are presented

--- assets/js/multigray.js
+++ assets/js/multigray.js
@@ -1,11 +1,11 @@
 (function() {
-  var boxes = document.getElementsByClassName("box");
+  var boxes = document.getElementsByClassName("multigray");
   var index = 0;
   var box_interval = setInterval(function() {
     if(index == boxes.length) {
       clearInterval(box_interval);
     } else {
-      boxes[index].style.backgroundColor = "rgba(0,0,0," + ((Math.random() / 2) + 0.25) + ")";
+      boxes[index].style.backgroundColor = "rgba(0,0,0," + ((Math.random() / 4) + 0.1) + ")";
     }
     index++;
   }, 30);
